ad55de12fb0ab906a9cc4ccb953d3e37be17a2aa
AlexandraFlorea06 <alexandra.florea06@stud.fiir.upb.ro>
2025-04-12 13:02:51 +0300

    CleanCode2 example2.c

--- example2.c
+++ example2.c
@@ -3,17 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//structura pentru un nod
 typedef struct Node {
     int data;
     struct Node *next;
 } NODE;
 
+//structura pentru graf
 typedef struct Graph {
     int vertices;
     int *visited;
     struct Node **adjacency_lists;
 } GRAPH;
 
+//crearea unui nod nou
 NODE *create_node(int value) {
     NODE *new_node = malloc(sizeof(NODE));
     new_node->data = value;
@@ -21,6 +24,7 @@ NODE *create_node(int value) {
     return new_node;
 }
 
+//crearea unui graf gol cu un nr de varfuri
 GRAPH *create_graph(int vertices) {
     int i;
     GRAPH *graph = malloc(sizeof(GRAPH));
@@ -28,12 +32,13 @@ GRAPH *create_graph(int vertices) {
     graph->adjacency_lists = malloc(vertices * sizeof(NODE *));
     graph->visited = malloc(sizeof(int) * vertices);
     for (int i = 0; i < vertices; i++) {
-        graph->adjacency_lists[i] = NULL;
-        graph->visited[i] = 0;
+        graph->adjacency_lists[i] = NULL; //nu are vecini initial
+        graph->visited[i] = 0; //niciun varf nu a fost vizitat
     }
     return graph;
 }
 
+//adaugarea unei muchii intre 2 noduri
 void add_edge(GRAPH *graph, int src, int dest) {
     NODE *new_node = create_node(dest);
 
@@ -46,7 +51,8 @@ void add_edge(GRAPH *graph, int src, int dest) {
     graph->adjacency_lists[dest] = new_node;
 }
 
-void insedg(int nr_of_vertices, int nr_of_edges, GRAPH *graph) {
+//introducerea muchiilor in graf
+void insert_edges(int nr_of_vertices, int nr_of_edges, GRAPH *graph) {
     int src, dest, i;
     printf("adauga %d muchii (de la 1 la %d)\n", nr_of_edges, nr_of_vertices);
     for (i = 0; i < nr_of_edges; i++) {
@@ -55,10 +61,12 @@ void insedg(int nr_of_vertices, int nr_of_edges, GRAPH *graph) {
     }
 }
 
+//verificare goala coada goala
 int is_empty(NODE *queue) {
     return queue == NULL;
 }
 
+//adaugare element in coada
 void enqueue(NODE **queue, int data) {
     NODE *new_node = create_node(data);
 
@@ -73,6 +81,7 @@ void enqueue(NODE **queue, int data) {
     }
 }
 
+//scoate primul element din coada
 int dequeue(NODE **queue) {
     int data = (*queue)->data;
     NODE *temp = *queue;
@@ -80,6 +89,7 @@ int dequeue(NODE **queue) {
     return data;
 }
 
+//afisare lista de adiacenta
 void print_graph(GRAPH *graph) {
     int i;
     for (i = 0; i < graph->vertices; i++) {
@@ -93,6 +103,7 @@ void print_graph(GRAPH *graph) {
     }
 }
 
+//afisare coada
 void print_queue(NODE *queue) {
     while (queue != NULL) {
         printf("%d ", queue->data);
@@ -100,7 +111,7 @@ void print_queue(NODE *queue) {
     }
 }
 
-
+//resetare vector de vizite (pentru rularea BFS dupa DSF)
 void wipe_visited_list(GRAPH *graph, int nr_of_vertices) {
     for (int i = 0; i < nr_of_vertices; i++) {
         graph->visited[i] = 0;
@@ -130,10 +141,16 @@ void BFS(GRAPH *graph, int start) {
 
     graph->visited[start] = 1;
     enqueue(&queue, start);
-
+    int first = 1;
     while (!is_empty(queue)) {
         int current = dequeue(&queue);
-        printf("%d ", current + 1);
+        if (first) {
+             printf("%d", current + 1);
+            first = 0;
+        }
+        else {
+            printf("->%d", current+1);
+        }
 
         NODE *temp = graph->adjacency_lists[current];
 
@@ -151,18 +168,20 @@ void BFS(GRAPH *graph, int start) {
 
 int main() {
     int nr_of_vertices, nr_of_edges, i, starting_vertex;
-    printf("cate noduri are graful? ");
+    printf("cate noduri are graful? "); //introducere nr noduri
     scanf("%d", &nr_of_vertices);
-    printf("cate muchii are graful? ");
+    printf("cate muchii are graful? "); //introducere muchii
     scanf("%d", &nr_of_edges);
     GRAPH *graph = create_graph(nr_of_vertices);
-    insedg(nr_of_vertices, nr_of_edges, graph);
+    insert_edges(nr_of_vertices, nr_of_edges, graph);
+    printf("\nReprezentarea grafului:\n");
+    print_graph(graph);
     printf("de unde plecam in DFS? ");
     scanf("%d", &starting_vertex);
     starting_vertex = starting_vertex - 1;
     printf("parcurgere cu DFS: ");
     DFS(graph, starting_vertex);
-    wipe_visited_list(graph, nr_of_vertices);
+    wipe_visited_list(graph, nr_of_vertices); //resetare vizite
     printf("\n");
     printf("de unde plecam in BFS? ");
     scanf("%d", &starting_vertex);
